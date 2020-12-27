89d04f2c79ece731e75d0fc64c39e0796858f580
Lester Lam <lesterlam@gmail.com>
2020-12-27 17:33:28 +0800

    pacman added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const width = 28;
 const grid = document.querySelector('.grid');
 const scoreDisplay = document.querySelector('#score');
 const squares = [];
+let locPacman = 490;
+let myScore = 0;
 
 //28 * 28 = 784
   // 0 - pac-dot
@@ -62,7 +64,63 @@ function createBoard() {
     grid.appendChild(square);
     squares.push(square);
   }
+  squares[locPacman].classList.add('pacman');
   // console.log(squares);
 }
 
+// get next position
+function getNextPosition(e) {
+  let to;
+  let from = locPacman;
+  switch(e.key) {
+    case "ArrowUp":
+      to = from - width;
+      break;
+    case "ArrowDown":
+      to = from + width;
+      break;
+    case "ArrowLeft":
+      if (from % width === 0) {
+        from += width;
+      }
+      to = from - 1;
+      break;
+    case "ArrowRight":
+        if ((from + 1) % width === 0) {
+          from -= width;
+        }
+      to = from + 1;
+      break;
+  }
+
+  if (to < 0 ||
+      to > squares.length ||
+      squares[to].classList.contains('wall') ||
+      squares[to].classList.contains('ghost-lair')) {
+    return locPacman;
+  }
+
+  return to;
+}
+
+// move to position
+function moveTo(to) {
+  squares[locPacman].classList.remove('pacman');
+  squares[to].classList.add('pacman');
+  locPacman = to;
+  if (squares[to].classList.contains('pac-dot') ||
+      squares[to].classList.contains('power-pellet')) {
+    squares[to].classList.remove('pac-dot');
+    squares[to].classList.remove('power-pellet');
+    myScore += 1;
+    scoreDisplay.innerHTML = myScore;
+  }
+}
+
+// control pacman
+function movePacman(e) {
+  moveTo(getNextPosition(e));
+}
+
 createBoard();
+document.addEventListener('keyup', movePacman);
