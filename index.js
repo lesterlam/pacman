fa67bad5540bc1adc7f6a5c88052391cb0ecb689
Lester Lam <lesterlam@gmail.com>
2020-12-27 20:02:07 +0800

    ghost movement added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const width = 28;
 const grid = document.querySelector('.grid');
 const scoreDisplay = document.querySelector('#score');
 const squares = [];
+const directions = [1, -1, width, -width];
 let locPacman = 490;
 let myScore = 0;
 
@@ -64,8 +65,56 @@ function createBoard() {
     grid.appendChild(square);
     squares.push(square);
   }
+
+  // setup pacman on board
   squares[locPacman].classList.add('pacman');
+
+  // setup ghosts on board
+  ghosts.forEach(g => {
+    squares[g.currentIndex].classList.add(g.name);
+    squares[g.currentIndex].classList.add('ghost');
+
+    g.timerId = setInterval(() => {
+      if (!checkGhostMove(g.currentIndex, g.direction)) {
+        let ghostDirections = getGhostDirections(g);
+        g.direction  = ghostDirections[Math.floor(Math.random() * ghostDirections.length)];
+      }
+      to = g.currentIndex + g.direction;
+      //remove ghost from current index
+      squares[g.currentIndex].classList.remove(g.name);
+      squares[g.currentIndex].classList.remove('ghost');
+      //update ghost current index
+      g.currentIndex += g.direction;
+      //add ghost to new index
+      squares[g.currentIndex].classList.add(g.name);
+      squares[g.currentIndex].classList.add('ghost');
+    }, g.speed);
+  })
   // console.log(squares);
+}
+
+// check whether it is valid ghost move
+function checkGhostMove(c, d) {
+  let to = c + d;
+  if (
+    !squares[to].classList.contains('wall') &&
+    !squares[to].classList.contains('ghost') &&
+    !(to % width === 0 && d === 1) &&
+    !((to + 1) % width === 0 && d === -1) &&
+    !(to <= 0) && !(to >= squares.length)
+  ) return true;
+  else return false;
+}
+
+// get ghost available directions
+function getGhostDirections(g) {
+  let ghostDirections = [];
+  directions.forEach( d => {
+    if (checkGhostMove(g.currentIndex, d)) {
+      ghostDirections.push(d)
+    }
+  })
+  return ghostDirections
 }
 
 // get next position
@@ -104,7 +153,7 @@ function getNextPosition(e) {
 }
 
 // move to position
-function moveTo(to) {
+function movePacmanTo(to) {
   squares[locPacman].classList.remove('pacman');
   squares[to].classList.add('pacman');
   locPacman = to;
@@ -119,8 +168,29 @@ function moveTo(to) {
 
 // control pacman
 function movePacman(e) {
-  moveTo(getNextPosition(e));
+  movePacmanTo(getNextPosition(e));
 }
+
+// ghost class
+class Ghost {
+  constructor(name, startIndex, speed) {
+    this.name = name;
+    this.startIndex = startIndex;
+    this.speed = speed;
+    this.currentIndex = startIndex
+    this.isScared = false;
+    this.timerId = NaN;
+    this.direction = directions[Math.floor(Math.random() * directions.length)]
+  }
+}
+
+// create ghosts list
+const ghosts = [
+  new Ghost('blinky', 348, 250),
+  new Ghost('pinky', 376, 400),
+  new Ghost('inky', 351, 300),
+  new Ghost('clyde', 379, 500)
+]
 
 createBoard();
 document.addEventListener('keyup', movePacman);
